2b74f5af4fe5ad9d9ac120accfd2d287f152a4ae
Ruben Sanchez Garcia <rubensanchezgarc@gmail.com>
2019-05-06 20:53:25 +0200

    conda installation added

--- conda/carbon_cleaner_em/build_conda_pckg.sh
+++ conda/carbon_cleaner_em/build_conda_pckg.sh
@@ -1,4 +1,4 @@
-conda-build ../carbon_cleaner_em/
+conda-build ../carbon_cleaner_em/ -c anaconda
 anaconda_path=`which python | rev | cut -f 5- -d"/" | rev`
 conda install --use-local carbon-cleaner-em
 anaconda upload $anaconda_path/conda-bld/linux-64/carbon-cleaner-em-*.bz2
