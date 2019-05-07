aa149b3cc7c78c148fb5d1ede6bd8a220f73fa62
Ruben Sanchez Garcia <rubensanchezgarc@gmail.com>
2019-05-07 15:44:48 +0200

    conda recipe fixed

--- conda/carbon_cleaner_em/build_conda_pckg.sh
+++ conda/carbon_cleaner_em/build_conda_pckg.sh
@@ -1,6 +1,6 @@
 conda-build ../carbon_cleaner_em/ -c anaconda
 anaconda_path=`which python | rev | cut -f 5- -d"/" | rev`
-conda install --use-local carbon-cleaner-em
+conda install --use-local carbon-cleaner-em -c anaconda
 anaconda upload $anaconda_path/conda-bld/linux-64/carbon-cleaner-em-*.bz2
 conda build purge
 
