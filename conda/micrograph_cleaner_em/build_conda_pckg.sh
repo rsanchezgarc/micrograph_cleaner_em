d2d170c7e2ce80174e9c5dac2ed4033f051422a3
Ruben Sanchez Garcia <rubensanchezgarc@gmail.com>
2019-10-16 17:53:48 -0400

    changes in conda build process

--- conda/micrograph_cleaner_em/build_conda_pckg.sh
+++ conda/micrograph_cleaner_em/build_conda_pckg.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-anaconda_path=`which python | rev | cut -f 5- -d"/" | rev`
+anaconda_path=`which python | rev | cut -f 3- -d"/" | rev`
 rm $anaconda_path/conda-bld/linux-64/micrograph-cleaner-em-*.bz2
-conda-build ../micrograph_cleaner_em/ -c anaconda
-#conda install --use-local micrograph-cleaner-em -c anaconda
+conda-build ../micrograph_cleaner_em/ -c anaconda -c conda-forge
+#conda install --use-local micrograph-cleaner-em -c anaconda -c conda-forge
 echo rsanchez1369 | anaconda upload $anaconda_path/conda-bld/linux-64/micrograph-cleaner-em-*.bz2   && conda build purge
 #xxxXxx
 
