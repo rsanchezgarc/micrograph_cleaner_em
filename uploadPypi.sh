ffecc96b17b0c5dd84e5498aa22c6c4710bdca78
Ruben Sanchez Garcia <rubensanchezgarc@gmail.com>
2019-10-22 19:13:28 -0400

    improved script

--- uploadPypi.sh
+++ uploadPypi.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+rm -r -f dist micrograph_cleaner_em.egg-info
 python setup.py sdist
 #pip install twine
-echo rsanchez1369 | twine upload dist/*
+echo rsanchez1369 | twine upload dist/*gz
