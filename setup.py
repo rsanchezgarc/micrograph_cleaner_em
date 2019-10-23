956ea80616ece64ecaff3e6fff4bfc00fb0baa36
Ruben Sanchez Garcia <rubensanchezgarc@gmail.com>
2019-10-22 21:34:39 -0400

    version 0.31

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys, os
 from setuptools import setup
 from subprocess import Popen, PIPE
 
-VERSION="0.30"
+VERSION="0.31"
 def readme():
   readmePath= os.path.abspath(os.path.join(__file__, "..", "README.md") )
   with open(readmePath) as f:
